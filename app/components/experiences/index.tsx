bae99dd0bbd15a4bf1453e189eeb2f08bcb2a98f
hodaknak <htoytoytok@gmail.com>
2024-12-10 02:42:38 -0800

    Format experiences

--- app/components/experiences/index.tsx
+++ app/components/experiences/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import styles from "./index.module.css";
 
 export default function Experiences() {
     return (
@@ -6,9 +7,27 @@ export default function Experiences() {
             <div className="flex flex-wrap w-full text-center">
                 <div className="w-1/2">
                     <p className="text-2xl">Work Experiences</p>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
                 </div>
                 <div className="w-1/2">
                     <p className="text-2xl">Projects</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+function Card() {
+    return (
+        <>
+            <div className="bg-[#474f69] shadow hover:bg-gray-700 rounded-lg w-1/2 m-auto border-2 mt-14 hover:scale-105" style={{transition: "all 1s ease"}}>
+                <div className="block p-6">
+                    <p className="mb-2 text-2xl font-bold tracking-tight text-gray-200">Education</p>
                 </div>
             </div>
         </>
